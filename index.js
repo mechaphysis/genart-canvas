c545667f8fee54d65b1bd92bd1caa1d336e27af7
Esperanza González <esperanza@mechaphysis.org>
2019-09-29 21:36:57 +0200

    feat: apply gaussian

--- index.js
+++ index.js
@@ -13,14 +13,18 @@ const sketch = () => {
       for (let y = 0; y < count; y++) {
         const i = x / (count - 1);
         const j = y / (count - 1);
-        points.push([i, j]);
+        points.push({
+          color: `hsl(0, ${random.value() * 100}%, 50%)`,
+          position: [i, j],
+          radius: Math.abs(random.gaussian()) // gaussian is a more 'organic' randomness
+        });
       }
     }
     return points;
   };
 
   // Set a deterministic seed for random, it can be a string or a number:
-  random.setSeed("asdfghsertyud");
+  random.setSeed("lorem ipsum");
   const points = createGrid(40).filter(() => random.value() > 0.5);
 
   /**
@@ -35,17 +39,21 @@ const sketch = () => {
     context.fillRect(0, 0, width, height);
 
     /**
-     * Loop over points array and deestructure each element, of the shape
-     * [i,j]
+     * Loop over points array and deestructure each element
      */
-    points.forEach(([i, j]) => {
+    points.forEach(pointData => {
+      const {
+        position: [i, j],
+        radius,
+        color
+      } = pointData;
       const x = lerp(margin, width - margin, i);
       const y = lerp(margin, height - margin, j);
 
       context.beginPath();
-      context.arc(x, y, 10, 0, Math.PI * 2, false);
-      context.strokeStyle = "gray";
-      context.lineWidth = 10;
+      context.arc(x, y, radius * width * 0.015, 0, Math.PI * 2, false);
+      context.fillStyle = "gray";
+      context.fill();
       context.stroke();
     });
   };
