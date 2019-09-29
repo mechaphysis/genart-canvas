110212d22281afe7b9e372a6474280dc6fb9510f
Esperanza González <esperanza@mechaphysis.org>
2019-09-29 21:43:40 +0200

    feat: integrate random nice palettes

--- index.js
+++ index.js
@@ -2,11 +2,15 @@
 import canvasSketch from "canvas-sketch";
 import { lerp } from "canvas-sketch-util/math";
 import random from "canvas-sketch-util/random";
+import palettes from "nice-color-palettes";
+
 const settings = {
   dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const palette = random.pick(palettes);
+
   const createGrid = count => {
     const points = [];
     for (let x = 0; x < count; x++) {
@@ -14,7 +18,7 @@ const sketch = () => {
         const i = x / (count - 1);
         const j = y / (count - 1);
         points.push({
-          color: `hsl(0, ${random.value() * 100}%, 50%)`,
+          color: random.pick(palette),
           position: [i, j],
           radius: Math.abs(random.gaussian()) // gaussian is a more 'organic' randomness
         });
@@ -35,7 +39,7 @@ const sketch = () => {
   const margin = 200;
   return ({ context, width, height }) => {
     // Change background to a nice dark-bluish gray
-    context.fillStyle = "hsl(227, 21%, 13%)";
+    //context.fillStyle = "hsl(227, 21%, 13%)";
     context.fillRect(0, 0, width, height);
 
     /**
@@ -52,7 +56,7 @@ const sketch = () => {
 
       context.beginPath();
       context.arc(x, y, radius * width * 0.015, 0, Math.PI * 2, false);
-      context.fillStyle = "gray";
+      context.fillStyle = color;
       context.fill();
       context.stroke();
     });
