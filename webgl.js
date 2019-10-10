1b5c13fedc8a966f3d8036147c04857922bfa323
Esperanza González <esperanza@mechaphysis.org>
2019-10-10 15:03:24 +0200

    refactor: remove lightning, bg to off-white

--- webgl.js
+++ webgl.js
@@ -1,16 +1,16 @@
 // Ensure ThreeJS is in global scope for the 'examples/'
-global.THREE = require('three');
+global.THREE = require("three");
 
 // Include any additional ThreeJS examples below
-require('three/examples/js/controls/OrbitControls');
+require("three/examples/js/controls/OrbitControls");
 
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
   // Make the loop animated
   animate: true,
   // Get a WebGL canvas rather than 2D
-  context: 'webgl',
+  context: "webgl",
   // Turn on MSAA
   attributes: { antialias: true }
 };
@@ -22,7 +22,7 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor('#000', 1);
+  renderer.setClearColor("hsl(0,0%,95%)", 1);
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
@@ -38,38 +38,30 @@ const sketch = ({ context }) => {
   const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1),
     new THREE.MeshPhysicalMaterial({
-      color: 'white',
+      color: "white",
       roughness: 0.75,
       flatShading: true
     })
   );
   scene.add(mesh);
 
-  // Specify an ambient/unlit colour
-  scene.add(new THREE.AmbientLight('#59314f'));
-
-  // Add some light
-  const light = new THREE.PointLight('#45caf7', 1, 15.5);
-  light.position.set(2, 2, -4).multiplyScalar(1.5);
-  scene.add(light);
-
   // draw each frame
   return {
     // Handle resize events here
-    resize ({ pixelRatio, viewportWidth, viewportHeight }) {
+    resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
       camera.aspect = viewportWidth / viewportHeight;
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
-    render ({ time }) {
-      mesh.rotation.y = time * (10 * Math.PI / 180);
+    render({ time }) {
+      mesh.rotation.y = time * ((10 * Math.PI) / 180);
       controls.update();
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
-    unload () {
+    unload() {
       controls.dispose();
       renderer.dispose();
     }
