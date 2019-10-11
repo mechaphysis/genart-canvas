c5f51ba815302042663643dd1890aaae826a3edf
Esperanza González <esperanza@mechaphysis.org>
2019-10-11 13:21:51 +0200

    feat: set ortographic perspective, generate set of cubic meshes

--- webgl.js
+++ webgl.js
@@ -25,22 +25,24 @@ const sketch = ({ context }) => {
   renderer.setClearColor("hsl(0,0%,95%)", 1);
 
   // Setup a camera
-  const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
-  camera.lookAt(new THREE.Vector3());
+  const camera = new THREE.OrthographicCamera();
 
   // Setup your scene
   const scene = new THREE.Scene();
 
-  const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial({
-      color: "green",
-      roughness: 0.75,
-      flatShading: true
-    })
-  );
-  scene.add(mesh);
+  for (let index = 0; index < 10; index++) {
+    const mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(1, 1, 1),
+      new THREE.MeshBasicMaterial({
+        color: "green",
+        roughness: 0.75,
+        flatShading: true
+      })
+    );
+    mesh.position.set(Math.random(), Math.random(), Math.random());
+    mesh.scale.multiplyScalar(0.1);
+    scene.add(mesh);
+  }
 
   // draw each frame
   return {
@@ -48,12 +50,29 @@ const sketch = ({ context }) => {
     resize({ pixelRatio, viewportWidth, viewportHeight }) {
       renderer.setPixelRatio(pixelRatio);
       renderer.setSize(viewportWidth, viewportHeight);
-      camera.aspect = viewportWidth / viewportHeight;
+
+      const aspect = viewportWidth / viewportHeight;
+
+      // Ortho zoom
+      const zoom = 1.0;
+
+      // Bounds
+      camera.left = -zoom * aspect;
+      camera.right = zoom * aspect;
+      camera.top = zoom;
+      camera.bottom = -zoom;
+
+      // Near/Far
+      camera.near = -100;
+      camera.far = 100;
+
+      // Set position & look at world center
+      camera.position.set(zoom, zoom, zoom);
+      camera.lookAt(new THREE.Vector3());
       camera.updateProjectionMatrix();
     },
     // Update & render your scene here
     render({ time }) {
-      mesh.rotation.y = time * ((10 * Math.PI) / 180);
       renderer.render(scene, camera);
     },
     // Dispose of events & renderer for cleaner hot-reloading
