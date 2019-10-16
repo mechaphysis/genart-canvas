afb1f0f61c8aab8c536634a9ca943c72e5a301bd
Esperanza González <esperanza@mechaphysis.org>
2019-10-16 10:25:14 +0200

    feat: create gradient

--- shader.js
+++ shader.js
@@ -16,8 +16,10 @@ const frag = glsl(/* glsl */ `
   varying vec2 vUv;
 
   void main () {
-    vec3 color = 0.5 + 0.5 * cos(time + vUv.xyx + vec3(0.0, 2.0, 4.0));
-    gl_FragColor = vec4(color, 1.0);
+    vec3 colorA = vec3(1.0, 0.0, 0.0);
+    vec3 colorB = vec3(0.0, 1.0, 0.0);
+    vec3 color = mix(colorA, colorB, vUv.x);
+    gl_FragColor = vec4(color, 1.0); 
   }
 `);
 
